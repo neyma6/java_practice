ded7c7918bca98aff8b570294476bb4e5eaf4c59
Gabor Csatlos <neyma6@gmail.com>
2015-09-26 13:21:52 +0200

    Test refactored

--- homework/src/test/java/com/nng/homework/io/StreetPlateParserTest.java
+++ homework/src/test/java/com/nng/homework/io/StreetPlateParserTest.java
@@ -2,6 +2,7 @@ package com.nng.homework.io;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,12 +10,12 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.nng.homework.domain.HouseNumberScheme;
 import com.nng.homework.domain.IStreetPlate;
 
 public class StreetPlateParserTest {
 	
-	public static final String[] VALID_INPUT = {"513563,390896,390897,\"4\",3,\"\",\"\",329,\"F\",\"NNNNNNN\",50,0,45,0,0,0,\"Budapesti\",\"út\",\"1\",\"\",\"O\",109,131,\"E\",104,140,\"Budaörs\",\"Budaörs\",\"\",\"\",244,244,\"2040\",\"2040\"", 
-												"513806,391093,391100,\"5.1\",3,\"\",\"\",514,\"F\",\"NNNNNNN\",50,0,40,0,0,0,\"Károly Király\",\"utca\",\"\",\"\",\"O\",25,83,\"E\",22,82,\"Budaörs\",\"Budaörs\",\"\",\"\",244,244,\"2040\",\"2040\""};
+	public static final String[] VALID_INPUT = {"513563,390896,390897,\"4\",3,\"\",\"\",329,\"F\",\"NNNNNNN\",50,0,45,0,0,0,\"Budapesti\",\"út\",\"1\",\"\",\"O\",109,131,\"E\",104,140,\"Budaörs\",\"Budaörs\",\"\",\"\",244,244,\"2040\",\"2040\""};
 	public static final String[] EMPTY_INPUT = {};
 	
 	public static final String[] INVALID_INPUT_WRONG_NUMBER = {"513563,390896,390897,\"4\",3,\"\",\"\",329,\"F\",\"NNNNNNN\",50,0,45,0,0,0,\"Budapesti\",\"út\",\"1\",\"\",\"O\",109,131,\"E\",test,140,\"Budaörs\",\"Budaörs\",\"\",\"\",244,244,\"2040\",\"2040\""};
@@ -33,7 +34,25 @@ public class StreetPlateParserTest {
 		List<IStreetPlate> sps = parser.parse(Arrays.asList(VALID_INPUT));
 		
 		assertFalse(sps.isEmpty());
-		assertTrue(sps.size() == 4);
+		assertTrue(sps.size() == 2);
+		
+		IStreetPlate left = sps.get(0);
+		IStreetPlate right = sps.get(1);
+		
+		assertEquals("Budapesti", left.getStreet().getName());
+		assertEquals("Budapesti", right.getStreet().getName());
+		
+		assertEquals("út", left.getStreet().getStreetType());
+		assertEquals("út", right.getStreet().getStreetType());
+		
+		assertEquals(HouseNumberScheme.ODD, left.getStreet().getNumberScheme());
+		assertEquals(HouseNumberScheme.EVEN, right.getStreet().getNumberScheme());
+		
+		assertEquals(109, left.getRange().getFrom());
+		assertEquals(104, right.getRange().getFrom());
+		
+		assertEquals(131, left.getRange().getTo());
+		assertEquals(140, right.getRange().getTo());
 	}
 	
 	@Test 
